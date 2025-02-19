1d4f81da515e4c701b1ddbe95343da0f1cf223c4
jonas <jonas.humenu@gmail.com>
2025-02-19 19:39:43 +0100

    Added my-listings page, custom dropdown funtionality, styling improvement

--- src/js/helper.js
+++ src/js/helper.js
@@ -11,3 +11,33 @@ if (performance.navigation.type === 1) {
 if (performance.getEntriesByType("navigation")[0]?.type === "reload") {
     console.log("Page was refreshed 2!");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownBtn = document.querySelector(".dropdown-btn");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    const selectedOption = document.getElementById("selected-option");
+    const hiddenInput = document.getElementById("dropdown-value");
+
+    dropdownBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        dropdownMenu.classList.toggle("show");
+        dropdownBtn.classList.toggle("active");
+    });
+
+    dropdownMenu.querySelectorAll("li").forEach((item) => {
+        item.addEventListener("click", function (e) {
+            e.stopPropagation();
+            selectedOption.textContent = this.textContent;
+            hiddenInput.value = this.dataset.value;
+            dropdownBtn.classList.remove("active");
+            dropdownMenu.classList.remove("show");
+        });
+    });
+
+    document.addEventListener("click", () => {
+        dropdownMenu.classList.remove("show");
+        dropdownBtn.classList.remove("active");
+    });
+});
+
+
